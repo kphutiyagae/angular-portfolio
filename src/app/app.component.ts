e15a5a525f7559345716d8bec4841a109c2bb486
kphutiyagae <kopanophutiyagae@gmail.com>
2026-09-10 18:09:18 +0200

    Wired up ionic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { IonApp, IonRouterOutlet } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [IonApp, IonRouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { IonCard } from '@ionic/angular';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [IonCard],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
+  standalone: true
 })
 export class LandingPageComponent {
 
